0169aee7b361a673e1568bb4911c4fa22e9fa7d9
baozhiqiang <brucebaozhiqiang@gmail.com>
2016-09-22 01:47:54 +0800

    完成knn算法

--- KNN/knn.py
+++ KNN/knn.py
@@ -6,7 +6,7 @@ import numpy as np
 import operator
 
 def createDataSet():
-	group = np.array([1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+	group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
 	labels = ['A','A','B','B']
 	return group, labels;
 
@@ -14,6 +14,17 @@ def classify0(inX, dataSet, labels,k):
 	dataSetSize = dataSet.shape[0];
 	diffMat = np.tile(inX, (dataSetSize,1)) - dataSet
 	sqDiffMat = diffMat ** 2
-	sqDistances = sqDiffMat.sum(axix = 1)
-	sqlDistances = sqDistances ** 0.5
-	
+	sqDistances = sqDiffMat.sum(axis = 1)
+	distances = sqDistances ** 0.5
+	sortedDistIndicies = distances.argsort()
+	classCount = {}
+	for i in range(k):
+		voteLabel = labels[sortedDistIndicies[i]]
+		classCount[voteLabel] = classCount.get(voteLabel,0) + 1
+	sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+	return sortedClassCount[0][0]
+
+if __name__ == '__main__':
+	group,labels = createDataSet()
+	test = classify0([0.2,0.2],group,labels,3)
+	print test
