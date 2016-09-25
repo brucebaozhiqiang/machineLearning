70f1bf0b35f95f33f9ac1d5c3dad7a138126368b
unknown <chirsbao@chirsbao-PC0.tencent.com>
2016-09-25 20:23:03 +0800

    comepelete dating_knn

---
+++ KNN/tempTest.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+created on 2016.09.28
+@author: bzq
+'''
+import numpy as np
+import operator
+def createDataSet():
+	group = np.array([[1.0,1.1],
+					  [1.0,1.0],
+					  [0,0],
+					  [0,0.1]])
+	labels = ['A','A','B','B']
+	return group, labels;
+
+group, label = createDataSet()
+
+minVals = group.min(0)
+
+print minVals
+
+maxVals = group.max(0)
+
+print maxVals
