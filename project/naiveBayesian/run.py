8a4de70ad91fab5dda497f4f482cc11d8ff083ad
bruce <bruce@bruces-Mac-mini.local>
2017-03-04 22:06:12 +0800

    add bayes

---
+++ project/naiveBayesian/run.py
@@ -0,0 +1,20 @@
+'''
+created on 2017.03.04
+@author: bzq
+'''
+import numpy as np
+import bayes
+
+
+if __name__ == '__main__':
+	from numpy import *
+	
+	listOPosts,listClasses=bayes.loadDataSet()
+
+	myVocabList = bayes.createVocabList(listOPosts)
+
+	trainMat=[]
+
+	for postinDoc in listOPosts:
+		trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
+		p0V,p1V,pAb=bayes.trainNB0(trainMat,listClasses)
