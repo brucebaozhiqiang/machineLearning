baf0dd149bae7ef8cc18440e208c995c3f4271cb
bruce <bruce@bruces-Mac-mini.local>
2017-03-08 22:19:12 +0800

    update

--- project/naiveBayesian/bayes.py
+++ project/naiveBayesian/bayes.py
@@ -10,18 +10,28 @@ def loadDataSet():
     classVec = [0,1,0,1,0,1] #1 代表侮辱性文字，0代表正常言论
     return postingList,classVec
 
+#设置词库
 def createVocabList(dataSet):
     vocabSet = set([])
     for document in dataSet:
         vocabSet = vocabSet | set(document)
     return list(vocabSet)
 
+#将数据转换成向量
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
         else: print "the word: %s is not in my Vocabulary!" %word
+    return returnVec
+
+#词袋模型
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in inputSet:
+            returnVec[vocabList.index(word)] += 1
     return returnVec
 
 def trainNB0(trainMatrix, trainCategory):
@@ -41,10 +51,73 @@ def trainNB0(trainMatrix, trainCategory):
     #print p1Num
     print "hello  %s " ,p1Denom
     p1Vect=[];p0Vect=[]
-    for i in range(len(p1Num)):?
-
-        p1Vect.append(p1Num[i]/p1Denom)
-        p0Vect.append(p0Num[i]/p0Denom)
+    for i in range(len(p1Num)):
+        #p1Vect.append(p1Num[i]/p1Denom) #change to log()
+        #p0Vect.append(p0Num[i]/p0Denom) #change to log()
+        p1Vect.append(log(p1Num/p1Denom))
+        p0Vect.append(log(p0Num/p0Denom))
 
     return p0Vect,p1Vect,pAbusive
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify*p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify*p0Vec) + log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat=[]
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V,p1V,pAb = trainNB0(array(trainMat), array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+    testEntry = ['stupid','garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\w*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) >2]
+
+def spamTest():
+    docList=[]; classList = []; fullText=[]
+    for i in range(1,26):
+        wordList= textParse(open('email/spam/%d.txt' %i).read())
+        #append 和extend 区别：append是追加  extend 是扩展
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt', %i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
