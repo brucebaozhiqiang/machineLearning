8e2b307886051207da22b7e36285b9b3c77d5414
bruce <bruce@bruces-Mac-mini.local>
2017-03-11 22:42:14 +0800

    update naiveBayes

--- project/naiveBayesian/bayes.py
+++ project/naiveBayesian/bayes.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from numpy import *
+import random
+
 def loadDataSet():
     postingList=[['my','dog','has','flea','problems','help','please'],
                  ['maybe','not','take','him','to','dog','park','stupid'],
@@ -90,15 +92,37 @@ def textParse(bigString):
 def spamTest():
     docList=[]; classList = []; fullText=[]
     for i in range(1,26):
-        wordList= textParse(open('email/spam/%d.txt' %i).read())
+        wordList= textParse(open('email/spam/%d.txt' % i).read())
         #append 和extend 区别：append是追加  extend 是扩展
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(1)
-        wordList = textParse(open('email/ham/%d.txt', %i).read())
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
+    vocabList = createVocabList(docList)
+    trainingSet=range(50); testSet=[]
+
+    #随机构建训练集
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+
+    trainMat = []; trainClasses=[]
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount += 1
+    print 'the error rate is: ', float(errorCount)/len(testSet)
 
 
 
--- project/naiveBayesian/run.py
+++ project/naiveBayesian/run.py
@@ -7,19 +7,21 @@ import bayes
 
 
 if __name__ == '__main__':
-	from numpy import *
-	
-	listOPosts,listClasses=bayes.loadDataSet()
+	# from numpy import *
+	# 
+	# listOPosts,listClasses=bayes.loadDataSet()
+# 
+	# myVocabList = bayes.createVocabList(listOPosts)
+# 
+	# print myVocabList
+# 
+	# trainMat=[]
+# 
+	# for postinDoc in listOPosts:
+	# 	trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
+	# p0V,p1V,pAb=bayes.trainNB0(trainMat,listClasses)
+# 
+	# #print pAb
+	# print p1V
 
-	myVocabList = bayes.createVocabList(listOPosts)
-
-	print myVocabList
-
-	trainMat=[]
-
-	for postinDoc in listOPosts:
-		trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
-	p0V,p1V,pAb=bayes.trainNB0(trainMat,listClasses)
-
-	#print pAb
-	print p1V
+	bayes.spamTest()
